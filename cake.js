d54447b4527d59929307b26252057232e865b82f
Balaji Kamal <124759238+balajikamal19@users.noreply.github.com>
2023-05-26 16:29:15 +0530

    Add files via upload

---
+++ cake.js
@@ -0,0 +1,7 @@
+function products(){  
+    document.getElementById("member").style.opacity = "0%";
+    document.querySelector(".product-img").style.filter = "brightness(1)";
+    document.getElementById("member-btn").style.boxShadow = "5px 10px 30px #70798C"
+
+
+}
